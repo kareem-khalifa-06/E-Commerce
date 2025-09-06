7b14b0e78fb69e6075a4c56253be38ff6bf5c5be
=eleng <=kareem.mohamed07@eng-st.cu.edu.eg>
2025-09-06 19:52:40 +0300

    mm

---
+++ src/Core/services/auth.service.ts
@@ -0,0 +1,16 @@
+import { HttpBackend, HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private _HttpClient:HttpClient) { }
+
+
+  // registerUser(userData:object):Observable<any>{
+  //  return this._HttpClient.post("Api-link",userData)
+  //}
+}
